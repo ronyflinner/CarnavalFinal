9ceab845515979481da70fc4b133a77105ecbf4d
luiskaco <luiskaco@gmail.com>
2018-12-18 16:33:15 -0500

    Cambios del menu

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -15,7 +15,7 @@
                     <nav class="classy-navbar justify-content-between" id="pixelNav">
 
                         <!-- Nav brand -->
-                        <a href="index.html" class="nav-brand" data-wow-delay="0.3s"><img src="{{ url('Carnaval1/img/logo-img/logo.png') }}" width="160px" height="90px" alt=""></a>
+                        <a href="{{ url('/') }}" class="nav-brand" data-wow-delay="0.3s"><img src="{{ url('Carnaval1/img/logo-img/logo.png') }}" width="160px" height="90px" alt=""></a>
 
                         <!-- Navbar Toggler -->
                         <div class="classy-navbar-toggler">
@@ -33,7 +33,7 @@
                              <!-- Nav Start -->
                             <div class="classynav">
                                 <ul>
-                                    <li><a href="index.html">Inicio &nbsp; &nbsp;</a></li>
+                                    <li><a href="{{ url('/') }}">Inicio &nbsp; &nbsp;</a></li>
                                     <li><a href="portfolio.html">Carnaval de São Paulo</a>
                                         <ul class="dropdown">
                                             <li><a href="index.html">Historia</a></li>
